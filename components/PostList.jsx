13c61482155de89b36753b32697b4185e915fa7b
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 18:15:50 +0530

    Create PostList.jsx

---
+++ components/PostList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PostList() {
+  return (
+    <div className="post-list">
+      <h3>Posts</h3>
+      <p>Feature coming soon with Firestore integration...</p>
+    </div>
+  );
+}
