bb7d60f9ac6ec8e5803075ee9a2e6b8665ef7a92
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 19:00:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
-import "./styles/main.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
